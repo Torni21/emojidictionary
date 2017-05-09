5f0ff34c2e14dcd3ff8a2668347f4d5c573bfc1a
tornike mindorashvili <tornikemindorashvili@tornikes-MacBook-Pro.local>
2017-05-08 20:28:59 -0400

    added the second view controller

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var mainTable: UITableView!
+    
+    var employees = ["Antonio", "Juan", "Tanya", "Pia", "Luis", "Hasna", "⛪️"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mainTable.dataSource = self
+        mainTable.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employees.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = employees[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
